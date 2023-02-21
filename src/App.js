ebab134eec0430a437b5eced2a19ebeff39fd1da
Ali Farahmand <alifarahmand1993@gmail.com>
2023-02-21 14:14:13 +0330

    completed adduserlist

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import React from 'react';
+import React,{useState} from 'react';
 import AddUser from './Components/Users/AddUser';
 import UsersList from './Components/Users/UsersList';
 
 
+
 function App() {
+
+  const [userList, setUserList] = useState([]);
+
+  const addUserHandler = (userName, UserAge) => {
+    setUserList((prevUserList) => {
+      return [...prevUserList, { name: userName, age: UserAge, id:Math.random().toString() }]
+    })
+  }
   return (
-    <>
-      <AddUser />
-      <UsersList users={[]}/>
-    </>
+    <div>
+      <AddUser onAddUser={addUserHandler}/>
+      <UsersList users={userList} />
+    </div>
   );
 }
 
