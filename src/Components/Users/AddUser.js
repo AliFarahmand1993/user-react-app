d59252076a30e9498c97a639556ad0920b5bea31
Ali Farahmand <alifarahmand1993@gmail.com>
2023-02-21 10:29:14 +0330

    add State for inputs

--- src/Components/Users/AddUser.js
+++ src/Components/Users/AddUser.js
@@ -1,26 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Button from '../Ui/Button';
 import Card from '../Ui/Card'
 import classes from './AddUser.module.css'
 
 const AddUser = (props) => {
+    const [enterdusername, setEnteredUsername] = useState('');
+    const [enterdَAge, setEnteredAge] = useState('');
 
     const addUserHandler = (event) => {
         event.preventDefault();
+        console.log(enterdusername, enterdَAge)
     };
 
+    const usernameChangeHandler = (event) => {
+        setEnteredUsername(event.target.value);
+    }
+    const ageChangeHandler = (event) => {
+        setEnteredAge(event.target.value);
+    }
 
-    return (
-        <Card className={classes.input}>
-            <form onClick={addUserHandler}>
-                <label htmlFor='username'>UserName</label>
-                <input id='username' type="text"></input>
-                <label htmlFor='age'>Age</label>
-                <input id='age' type="number"></input>
-                <Button type='submit'>Add User</Button>
-            </form>
-        </Card>
-    )
+return (
+    <Card className={classes.input}>
+        <form onClick={addUserHandler}>
+            <label htmlFor='username'>UserName</label>
+            <input
+                id='username'
+                type="text"
+                onChange={usernameChangeHandler}></input>
+            <label htmlFor='age'>Age</label>
+            <input
+                id='age'
+                type="number"
+                onChange={ageChangeHandler}></input>
+            <Button type='submit'>Add User</Button>
+        </form>
+    </Card>
+)
 }
 
 export default AddUser
