6ec1192297aedb06cdc97e5c668686c0401a3dd7
Ali Farahmand <alifarahmand1993@gmail.com>
2023-02-21 11:09:02 +0330

    Update AddUser.js
    fix bug -- add onSubmit
    add condition for input

--- src/Components/Users/AddUser.js
+++ src/Components/Users/AddUser.js
@@ -4,12 +4,21 @@ import Card from '../Ui/Card'
 import classes from './AddUser.module.css'
 
 const AddUser = (props) => {
-    const [enterdusername, setEnteredUsername] = useState('');
-    const [enterdَAge, setEnteredAge] = useState('');
+    const [enteredUsername, setEnteredUsername] = useState('');
+    const [enteredAge, setEnteredAge] = useState('');
 
     const addUserHandler = (event) => {
         event.preventDefault();
-        console.log(enterdusername, enterdَAge)
+        if(enteredUsername.trim().length ===  0 || enteredAge.trim().length === 0){
+            return;
+        }
+        if(+enteredAge < 1) {
+            return;
+        }
+        console.log(enteredUsername, enteredAge);
+        setEnteredUsername('');
+        setEnteredAge('');
+
     };
 
     const usernameChangeHandler = (event) => {
@@ -19,23 +28,25 @@ const AddUser = (props) => {
         setEnteredAge(event.target.value);
     }
 
-return (
-    <Card className={classes.input}>
-        <form onClick={addUserHandler}>
-            <label htmlFor='username'>UserName</label>
-            <input
-                id='username'
-                type="text"
-                onChange={usernameChangeHandler}></input>
-            <label htmlFor='age'>Age</label>
-            <input
-                id='age'
-                type="number"
-                onChange={ageChangeHandler}></input>
-            <Button type='submit'>Add User</Button>
-        </form>
-    </Card>
-)
+    return (
+        <Card className={classes.input}>
+            <form onSubmit={addUserHandler}>
+                <label htmlFor='username'>UserName</label>
+                <input
+                    id='username'
+                    type="text"
+                    value={enteredUsername}
+                    onChange={usernameChangeHandler}></input>
+                <label htmlFor='age'>Age</label>
+                <input
+                    id='age'
+                    type="number"
+                    value={enteredAge}
+                    onChange={ageChangeHandler}></input>
+                <Button type='submit'>Add User</Button>
+            </form>
+        </Card>
+    )
 }
 
 export default AddUser
